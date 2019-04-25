ff4d6b6cb91e8d7b9e48a4128a46dc91e13f9810
Duozzh <49937168+Duozzh@users.noreply.github.com>
2019-04-25 20:19:23 +0800

    Add files via upload

---
+++ Thread/ThreadTest.java
@@ -0,0 +1,28 @@
+package Thread;
+
+public class ThreadTest {
+	public static void main(String[] args) {
+		Thread1 t1 = new Thread1();
+		Thread2 t2 = new Thread2();
+		
+		t1.start();
+		t2.start();
+	}
+}
+ class Thread1 extends Thread{
+	 @Override
+	public void run() {
+	for(int i = 0;i<100;i++) {
+		System.out.println("hello:"+i);
+	}
+	}
+ }
+ class Thread2 extends Thread{
+	 @Override
+	public void run() {
+	for(int i = 0;i<100;i++) {
+		System.out.println("world:"+i);
+	}
+	}
+ }
+ 
